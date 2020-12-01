744606994bb3c89d9a34b5a6976a6d949b8ba69a
Jerzy <jerzdb@gmail.com>
2020-12-01 10:31:08 -0500

    First commit

---
+++ Dobrowolski_p5/src/ContactListTest.java
@@ -0,0 +1,14 @@
+public class ContactListTest
+{
+    addingItemsIncreasesSize()
+    editingItemsFailsWithAllBlankValues()
+    editingItemsFailsWithInvalidIndex()
+    editingSucceedsWithBlankFirstName()
+    editingSucceedsWithBlankLastName()
+    editingSucceedsWithBlankPhone()
+    editingSucceedsWithNonBlankValues()
+    newListIsEmpty()
+    removingItemsDecreasesSize()
+    removingItemsFailsWithInvalidIndex()
+    savedContactListCanBeLoaded()
+}
---
+++ Dobrowolski_p5/src/TaskItemTest.java
@@ -0,0 +1,13 @@
+public class TaskItemTest
+{
+    constructorFailsWithInvalidDueDate()
+    constructorFailsWithInvalidTitle()
+    constructorSucceedsWithValidDueDate()
+    constructorSucceedsWithValidTitle()
+    editingDescriptionSucceedsWithExpectedValue()
+    editingDueDateFailsWithInvalidDateFormat()
+    editingDueDateFailsWithInvalidYYYMMDD()
+    editingDueDateSucceedsWithExpectedValue()
+    editingTitleFailsWithEmptyString()
+    editingTitleSucceedsWithExpectedValue()
+}
